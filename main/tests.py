17824af5e119b85027c12d66119a2fe302838d77
alyssasaaa <alyssa.rahma@ui.ac.id>
2026-09-14 23:45:29 +0700

    feat: complete assignment 2, add changes to tests.py, add README.md

--- main/tests.py
+++ main/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from main.models import Experience
+from main.models import Education, Experience
 
 
 class MainTest(TestCase):
@@ -46,7 +46,14 @@ class MainTest(TestCase):
         Experience.objects.all().delete()
         response = self.client.get(reverse("main:show_experience"))
 
-        self.assertContains(response, "No experience has been added yet.")
+        self.assertContains(
+            response,
+            "No university experience has been added yet.",
+        )
+        self.assertContains(
+            response,
+            "No high school experience has been added yet.",
+        )
 
     def test_completed_experience(self):
         self.experience.ended_at = timezone.now()
@@ -56,3 +63,40 @@ class MainTest(TestCase):
         self.assertFalse(self.experience.is_ongoing)
         self.assertContains(response, "Completed")
         self.assertNotContains(response, "Ongoing")
+
+class EducationTest(TestCase):
+    def setUp(self):
+        self.education = Education.objects.create(
+            institution="Universitas Indonesia",
+            program="Computer Science International Class Program",
+            description="Studying Computer Science at Universitas Indonesia.",
+            start_year=2025,
+            end_year=None,
+            image="img/sasa-uni.png",
+            skills="Python, Figma, Data Science",
+        )
+
+    def test_education_url_is_accessible_and_uses_correct_template(self):
+        response = self.client.get(reverse("main:show_education"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "education.html")
+
+    def test_education_data_appears_in_response(self):
+        response = self.client.get(reverse("main:show_education"))
+
+        self.assertContains(response, self.education.institution)
+        self.assertContains(response, self.education.program)
+        self.assertContains(response, self.education.description)
+        self.assertContains(response, "Python")
+        self.assertContains(response, "present")
+
+    def test_empty_education_message_appears(self):
+        Education.objects.all().delete()
+
+        response = self.client.get(reverse("main:show_education"))
+
+        self.assertContains(
+            response,
+            "No education has been added yet.",
+        )
