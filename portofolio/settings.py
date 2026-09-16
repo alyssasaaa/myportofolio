c7ea23f842a197199813b2baaf7ed1256486eec1
alyssasaaa <alyssa.rahma@ui.ac.id>
2026-09-16 20:38:25 +0700

    feat: complete tutorial 03 form and data delivery

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -149,3 +149,6 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+# gunakan https:// untuk trailing urlnya
+CSRF_TRUSTED_ORIGINS = ["https://alyssa-rahma-myportofolio.pws.cs.ui.ac.id/"]
